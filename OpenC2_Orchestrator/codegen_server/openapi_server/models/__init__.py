9d13ba5e8b93468304992582af76be27ecbd3eeb
Ekam Puri Nieto <ekam.purin@um.es>
2024-09-26 10:45:59 +0200

    Remove OpenC2 files
    Refer to the OpenC2 repository for the contents
    
    Signed-off-by: Ekam Puri Nieto <ekam.purin@um.es>

--- OpenC2_Orchestrator/codegen_server/openapi_server/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-# import models into model package
-from openapi_server.models.close_conn_info import CloseConnInfo
-from openapi_server.models.delete_file_info import DeleteFileInfo
-from openapi_server.models.error_message import ErrorMessage
-from openapi_server.models.open_c2_command_headers import OpenC2CommandHeaders
-from openapi_server.models.open_c2_command_response import OpenC2CommandResponse
-from openapi_server.models.open_c2_response import OpenC2Response
