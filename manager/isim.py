e41f6ddc7732208238349c713a0fa6344cba6a67
Ekam Puri Nieto <ekam.purin@um.es>
2024-11-05 10:15:46 +0100

    Linting and docstrings
    Signed-off-by: Ekam Puri Nieto <ekam.purin@um.es>

--- manager/isim.py
+++ manager/isim.py
@@ -1,22 +1,30 @@
+from collections.abc import Callable
 from typing import LiteralString
-from typing_extensions import Callable
+
 from neo4j import AsyncDriver, Query, Record
 
 DRIVER: AsyncDriver | None = None
 
+
 def set_driver(driver: AsyncDriver):
+    """Set the global ISIM driver."""
     global DRIVER
     DRIVER = driver
 
+
 def get_driver() -> AsyncDriver:
+    """Get the global ISIM driver."""
     global DRIVER
     if DRIVER is None:
-        raise Exception('Neo4j driver was never set')
+        msg = 'Neo4j driver was never set'
+        raise Exception(msg)
     return DRIVER
 
 
 async def check_condition(query: LiteralString,
                           parameters: dict,
-                          condition: Callable[[list[Record], dict], bool]):
+                          condition: Callable[[list[Record], dict], bool],
+                          ) -> bool:
+    """Query the ISIM and evaluate a condition."""
     res = await get_driver().execute_query(Query(query), parameters)
     return condition(res.records, parameters)
