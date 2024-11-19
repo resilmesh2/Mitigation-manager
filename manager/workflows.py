7fda8422863eec89fee075922aec80b47d5d839d
Ekam Puri Nieto <ekam.purin@um.es>
2024-11-19 12:25:44 +0100

    Remove obsolete function
    Signed-off-by: Ekam Puri Nieto <ekam.purin@um.es>

--- manager/workflows.py
+++ manager/workflows.py
@@ -10,34 +10,3 @@ async def locate(node: AttackNode) -> Workflow | None:
         return None
     # Current technique: get the lowest cost workflow
     return sorted(valid_workflows, key=lambda w: w.cost)[0]
-
-
-async def get() -> list[dict]:
-    return [
-        {
-            'name': 'delete_file',
-            'webhook': '6b219a4d-9723-4607-b6c6-6e56f790650c',
-            'performs': ['D3-FEV'],
-            'mitigates': ['T1204.002'],
-            'set_cost': 1,
-        },
-        {
-            'name': 'close_conn',
-            'type': ['D3-PT', 'D3-ST'],
-            'webhook': 'aa2e31ea-dd3e-4471-ad4e-3f032bdb381d',
-            'attacks': ['T1041', 'T1219'],
-            'set_cost': 10,
-        },
-        {
-            'name': 'handle_ransomware',
-            'webhook': '1d5366eb-8006-45a3-8fff-e764c283b811',
-            'attacks': ['T1204.002'],
-            'set_cost': 5,
-        },
-        {
-            'name': 'shutdown_interface',
-            'webhook': None,
-            'attacks': ['T1041, T1219'],
-            'set_cost': 5,
-        },
-    ]
